715d0b618a67f0b6fdb3bb82fe2a0fbf0ab208f2
Eric <info@ericparlier.com>
2023-02-17 14:05:10 -0500

    Removed obsolete dir from install

--- install.js
+++ install.js
@@ -1,18 +1,14 @@
 const path = require('path');
 const fs = require('fs-extra');
 const copy = require('recursive-copy');
-const src_ogx = path.normalize(__dirname+'/ogx');
 const src_www = path.normalize(__dirname+'/www');
 const src_themes = path.normalize(__dirname+'/www/themes');
 const src_bin = path.normalize(__dirname+'/www/js/bin');
 const src_lib = path.normalize(__dirname+'/www/js/lib');
 const dest_www = path.normalize(__dirname+'./../../../www');
 const dest_themes = path.normalize(__dirname+'./../../../www/themes');
-const dest_ogx = path.normalize(__dirname+'./../../../ogx');
 const dest_lib = path.normalize(__dirname+'./../../js/lib');
 const dest_bin = path.normalize(__dirname+'./../../js/bin');
-//copy ogx folder
-copy(src_ogx, dest_ogx, {overwrite:true});
 //if no www, copy the whole folder
 if(!fs.existsSync(dest_www)){
     copy(src_www, dest_www);
