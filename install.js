9d0eef7a41fe01b29f2b1c287463c3a74e5ce6d8
Eric <info@ericparlier.com>
2025-02-06 08:07:15 +0100

    1.46.0-dev

--- install.js
+++ install.js
@@ -1,3 +1,4 @@
+const root = require.main.paths[0].split('node_modules')[0].slice(0, -1);
 const path = require('path');
 const fs = require('fs-extra');
 const copy = require('recursive-copy');
@@ -5,10 +6,10 @@ const src_www = path.normalize(__dirname+'/www');
 const src_themes = path.normalize(__dirname+'/www/themes');
 const src_bin = path.normalize(__dirname+'/www/js/bin');
 const src_lib = path.normalize(__dirname+'/www/js/lib');
-const dest_www = path.normalize(__dirname+'./../../../www');
-const dest_themes = path.normalize(__dirname+'./../../../www/themes');
-const dest_lib = path.normalize(__dirname+'./../../js/lib');
-const dest_bin = path.normalize(__dirname+'./../../js/bin');
+const dest_www = path.normalize(root+'/www');
+const dest_themes = path.normalize(root+'/www/themes');
+const dest_lib = path.normalize(root+'/www/js/lib');
+const dest_bin = path.normalize(root+'/www/js/bin');
 //if no www, copy the whole folder
 if(!fs.existsSync(dest_www)){
     copy(src_www, dest_www);
@@ -17,30 +18,34 @@ if(!fs.existsSync(dest_www)){
     console.log('Info: www present, merging files...');
     //if www, check if js and js/lib already there
     if(!fs.existsSync(dest_lib)){
+        console.log('Info: copying lib folder');    
         copy(src_lib, dest_lib);        
-    }else{        
+    }else{    
+        console.log('Info: merging libraries...');           
         //copy cryto, globules, howler, jquery, moment, mongogx
         let lib_folders = ['cryto', 'globules', 'howler', 'jquery', 'moment', 'mongogx'];
         let lib_folders_src;
         let lib_folders_dest;
         for(let i = 0; i < lib_folders.length; i++){
             lib_folders_src =  path.normalize(__dirname+'/www/js/lib/'+lib_folders[i]);
-            lib_folders_dest = path.normalize(__dirname+'./../../js/lib/'+lib_folders[i]);
+            lib_folders_dest = path.normalize(dest_www+'/js/lib/'+lib_folders[i]);
             //patch --dev
             if(fs.existsSync(lib_folders_src)){
                 console.log('Info: merging lib', lib_folders[i]);
                 copy(lib_folders_src, lib_folders_dest, {overwrite:true});
             }
-        }        
+        }  
+        console.log('Info: done merging libraries');        
     }   
     //check if themes are here
     //patch --dev
     if(!fs.existsSync(dest_themes) && fs.existsSync(src_themes)){      
+        console.log('Info: copying default theme');       
         copy(src_themes, dest_themes, {overwrite:false});        
     }
     //deploy js/bin if not already there
     //patch --dev
-    if(!fs.existsSync(dest_bin) && fs.existsSync(src_bin)){      
+    if(!fs.existsSync(dest_bin) && fs.existsSync(src_bin)){         
         copy(src_bin, dest_bin, {overwrite:false});        
     } 
 }
